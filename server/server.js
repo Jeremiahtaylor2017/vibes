d1e59d95cb5350e28671414862fc3af92d13c5e2
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2022-12-12 01:19:29 -0800

    finished backend

--- server/server.js
+++ server/server.js
@@ -9,7 +9,18 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import { verifyToken } from './middleware/auth.js';
+
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+
 import { register } from './controllers/auth.js';
+import { createPost } from './controllers/posts.js';
+
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -38,12 +49,23 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+// ROUTES
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL)
 mongoose.connection
-    .on("open", () => console.log("Connected to MongoDB"))
+    .on("open", () => {
+        console.log("Connected to MongoDB")
+        // ADD DATA ONE TIME
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+    })
     .on("close", () => console.log("Disconnected from MongoDB"))
     .on("error", () => console.log(err.message));
 
